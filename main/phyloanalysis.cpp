xcomputing-XGPhy/iqtree3

commit 9af0c6a44192f14499d05952ee578611aa675e28

Changed how BIONJ et cetera trees are exposed to other parts of iqtree. Now done through StartTree::Factory, defined in utils/starttree.h and implemented in utils/starttree.cpp. -starttree parameters NJ, NJ-R, NJ-V, BIONJ, BIONJ-R, BIONJ-V are "advertised" in utils/bionj.cpp (via addBioNJ2020TreeBuilders).  Renamed bionj.h to bionj.cpp (since it isn't really a header). Added an adapter (BIONJ2009Adapter) to make BioNj look like one of the other tree builders.
Alignment::checkSeqName() will now suppress list of sequences if --suppress-list-of-sequences parameter passed. PhyloTree::removeIdenticalSeqs will now suppress list of indentical sequences if --suppress-duplicate-sequence" parameter passed. computeInitialDist() corrected typo in logged line (mentioning Jukes-Cantor).  Simplified #pragma omp stuff in bionj.cpp (just use #pragma amp parallel for).

main/phyloanalysis.cpp
@@ -1663,7 +1663,7 @@ void computeInitialDist(Params &params, IQTree &iqtree) {
     if (params.dist_file) {
         cout << "Reading distance matrix file " << params.dist_file << " ..." << endl;
     } else if (params.compute_jc_dist) {
-        cout << "Computing Juke-Cantor distances..." << endl;
+        cout << "Computing Jukes-Cantor distances..." << endl;
     } else if (params.compute_obs_dist) {
         cout << "Computing observed distances..." << endl;
     }
@@ -1671,9 +1671,10 @@ void computeInitialDist(Params &params, IQTree &iqtree) {
     if (params.compute_jc_dist || params.compute_obs_dist || params.partition_file) {
         double begin_time = getRealTime();
         longest_dist = iqtree.computeDist(params, iqtree.aln, iqtree.dist_matrix, iqtree.var_matrix, iqtree.dist_file);
-        //checkZeroDist(iqtree.aln, iqtree.dist_matrix);
         cout << "Distance calculation time: " << getRealTime() - begin_time << " seconds" << endl;
-
+        //if (!params.suppress_zero_distance_warnings) {
+        //  checkZeroDist(iqtree.aln, iqtree.dist_matrix);
+        //}
         double max_genetic_dist = MAX_GENETIC_DIST;
         if (iqtree.aln->seq_type == SEQ_POMO) {
             int N = iqtree.aln->virtual_pop_size;

alignment/alignment.cpp
@@ -185,9 +185,7 @@ void Alignment::checkSeqName() {
     if (seq_type == SEQ_POMO)
         cout << "NOTE: The composition test for PoMo only tests the proportion of fixed states!" << endl;
 
-    bool listSequences = true;
-        //Todo: Add parameter to disable...
-        //listing of sequences (and set from that)
+    bool listSequences = !Params::getInstance().suppress_list_of_sequences;
     
     int max_len = getMaxSeqNameLength()+1;
     if (listSequences) {
@@ -384,7 +382,6 @@ Alignment *Alignment::removeIdenticalSeq(string not_remove, bool keep_two, StrVe
         cout << "Checking for duplicate sequences took " << checkTime
             << " wall-clock seconds" << endl;
     }
-
 	if (removed_seqs.size() > 0) {
 		if (removed_seqs.size() >= getNSeq()-3)
 			outWarning("Your alignment contains too many identical sequences!");
@@ -398,7 +395,6 @@ Alignment *Alignment::removeIdenticalSeq(string not_remove, bool keep_two, StrVe
 		return aln;
 	} else return this;
 }
-
 
 bool Alignment::isGapOnlySeq(int seq_id) {
     ASSERT(seq_id < getNSeq());
@@ -3879,7 +3875,6 @@ double Alignment::computeObsDist(int seq1, int seq2) {
         int state1 = convertPomoState((*it)[seq1]);
         int state2 = convertPomoState((*it)[seq2]);
         if  (state1 < num_states && state2 < num_states) {
-            //if ((*it)[seq1] != STATE_UNKNOWN && (*it)[seq2] != STATE_UNKNOWN) {
             total_pos += (*it).frequency;
             if (state1 != state2 )
                 diff_pos += (*it).frequency;
@@ -3897,16 +3892,9 @@ double Alignment::computeJCDist(int seq1, int seq2) {
     double obs_dist = computeObsDist(seq1, seq2);
     double z = (double)num_states / (num_states-1);
     double x = 1.0 - (z * obs_dist);
-
     if (x <= 0) {
-        // string str = "Too long distance between two sequences ";
-        // str += getSeqName(seq1);
-        // str += " and ";
-        // str += getSeqName(seq2);
-        // outWarning(str);
         return MAX_GENETIC_DIST;
     }
-
     return -log(x) / z;
 }
 

tree/phylotree.cpp
@@ -20,7 +20,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 #include "phylotree.h"
-#include "utils/bionj.h"
+#include "utils/starttree.h"
 //#include "rateheterogeneity.h"
 #include "alignment/alignmentpairwise.h"
 #include <algorithm>
@@ -3018,7 +3018,6 @@ double PhyloTree::addTaxonML(Node *added_node, Node* &target_node, Node* &target
 }
 
 void PhyloTree::growTreeML(Alignment *alignment) {
-
     cout << "Stepwise addition using ML..." << endl;
     aln = alignment;
     int size = aln->getNSeq();
@@ -3204,8 +3203,6 @@ double PhyloTree::computeDist(Params &params, Alignment *alignment, double* &dis
         dist_mat = new double[alignment->getNSeq() * alignment->getNSeq()];
         memset(dist_mat, 0, sizeof(double) * alignment->getNSeq() * alignment->getNSeq());
         var_mat = new double[alignment->getNSeq() * alignment->getNSeq()];
-        // BUG!
-        //memset(var_mat, 1, sizeof(double) * alignment->getNSeq() * alignment->getNSeq());
         int nseq = alignment->getNSeq();
         for (int i = 0; i < nseq; i++)
             for (int j = 0; j < nseq; j++)
@@ -3264,20 +3261,18 @@ double PhyloTree::computeObsDist(Params &params, Alignment *alignment, double* &
 void PhyloTree::computeBioNJ(Params &params, Alignment *alignment, string &dist_file) {
     string bionj_file = params.out_prefix;
     bionj_file += ".bionj";
-    cout << "Computing BIONJ tree..." << endl;
-    BioNj bionj;
-    bionj.create(dist_file.c_str(), bionj_file.c_str());
-//    bool my_rooted = false;
+    auto treeBuilder
+        = StartTree::Factory::getTreeBuilderByName
+            ( params.start_tree_subtype_name);
+    cout << "Computing " << treeBuilder->getName() << " tree"
+        << "(in file " << bionj_file << ")... from distance matrix"
+        << "(in " << dist_file << ")" << endl;
+    treeBuilder->constructTree(dist_file, bionj_file);
     bool non_empty_tree = (root != NULL);
-//    if (root)
-//        freeNode();
     readTreeFile(bionj_file.c_str());
-    
-
     if (non_empty_tree) {
         initializeAllPartialLh();
     }
-//    setAlignment(alignment);
 }
 
 int PhyloTree::setNegativeBranch(bool force, double newlen, Node *node, Node *dad) {
@@ -5121,7 +5116,9 @@ void PhyloTree::removeIdenticalSeqs(Params &params) {
     if (removed_seqs.size() > 0) {
         cout << "NOTE: " << removed_seqs.size() << " identical sequences (see below) will be ignored for subsequent analysis" << endl;
         for (int i = 0; i < removed_seqs.size(); i++) {
-            cout << "NOTE: " << removed_seqs[i] << " (identical to " << twin_seqs[i] << ") is ignored but added at the end" << endl;
+            if (!params.suppress_duplicate_sequence_warnings) {
+                cout << "NOTE: " << removed_seqs[i] << " (identical to " << twin_seqs[i] << ") is ignored but added at the end" << endl;
+            }
         }
         delete aln;
         aln = new_aln;

utils/CMakeLists.txt
@@ -7,8 +7,9 @@ tools.cpp tools.h
 pllnni.cpp pllnni.h
 checkpoint.cpp checkpoint.h
 MPIHelper.cpp MPIHelper.h
-timeutil.h bionj.h
-bionj2.cpp bionj2.h
+starttree.cpp starttree.h
+bionj.cpp bionj2.cpp
+timeutil.h
 )
 
 if(ZLIB_FOUND)

utils/bionj.cpp
@@ -1,5 +1,3 @@
-#ifndef BIONJ_H
-#define BIONJ_H
 /*;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;                                                                           ;
 ;                         BIONJ program                                     ;
@@ -23,13 +21,12 @@
     #define NJ_OMP
 #endif
 
-
 #include <stdio.h>                  
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include "utils/timeutil.h" //JB2020-06-18 for getRealTime()
-
+#include "starttree.h"
 
 #define PREC 8                             /* precision of branch-lengths  */
 #define PRC  100
@@ -397,27 +394,22 @@ float Sum_S(int i, float **delta)          /* get sum Si form the diagonal */
     void Compute_sums_Sx(float **delta, int n)
     {
         #ifdef NJ_OMP
-            #pragma omp parallel
+            #pragma omp parallel for
         #endif
+        for(int i= 1; i <= n ; i++)
         {
-            #ifdef NJ_OMP
-                #pragma omp for
-            #endif
-            for(int i= 1; i <= n ; i++)
+            if(!Emptied(i,delta))
             {
-                if(!Emptied(i,delta))
+                float sum=0;
+                for(int j=1; j <=n; j++)
                 {
-                    float sum=0;
-                    for(int j=1; j <=n; j++)
-                    {
-                        if(i != j && !Emptied(j,delta))           /* compute the sum Si */
-                            sum=sum + Distance(i,j,delta);
-                    }
-                    delta[i][i]=sum;                           /* store the sum Si in */
+                    if(i != j && !Emptied(j,delta))           /* compute the sum Si */
+                        sum=sum + Distance(i,j,delta);
                 }
-            }                                               /* delta�s diagonal    */
-        }
-    }
+                delta[i][i]=sum;                           /* store the sum Si in */
+            }
+        }                                               /* delta�s diagonal    */
+}
 
 
 
@@ -447,36 +439,31 @@ float Sum_S(int i, float **delta)          /* get sum Si form the diagonal */
         int    step = (n<240) ? 1 : (n / 120);//JB2020-06-17 Blocks of adjacent rows
         
         #ifdef NJ_OMP
-            #pragma omp parallel
+            #pragma omp parallel for
         #endif
-        {
-            #ifdef NJ_OMP
-                #pragma omp for
-            #endif
-            for (int w=2; w<=n; w+=step) {              //JB2020-06-17
-                int w2 = (w+step <= n) ? (w+step) : n;  //JB2020-06-17
-                for (int x=w; x <= w2; ++x)             //JB2020-06-17
+        for (int w=2; w<=n; w+=step) {              //JB2020-06-17
+            int w2 = (w+step <= n) ? (w+step) : n;  //JB2020-06-17
+            for (int x=w; x <= w2; ++x)             //JB2020-06-17
+            {
+                float rowMin = Qmin;
+                int   bestY  = 0;
+                if(!Emptied(x,delta))
                 {
-                    float rowMin = Qmin;
-                    int   bestY  = 0;
-                    if(!Emptied(x,delta))
+                    for(int y=1; y < x; ++y)
                     {
-                        for(int y=1; y < x; ++y)
+                        if(!Emptied(y,delta))
                         {
-                            if(!Emptied(y,delta))
+                            float Qxy=Agglomerative_criterion(x,y,delta,r);
+                            if(Qxy < rowMin-0.000001)
                             {
-                                float Qxy=Agglomerative_criterion(x,y,delta,r);
-                                if(Qxy < rowMin-0.000001)
-                                {
-                                    rowMin=Qxy;
-                                    bestY = y;
-                                }
+                                rowMin=Qxy;
+                                bestY = y;
                             }
                         }
                     }
-                    rowQmin[x] = rowMin;
-                    rowMinColumn[x] = bestY;
                 }
+                rowQmin[x] = rowMin;
+                rowMinColumn[x] = bestY;
             }
         }
         //JB2020-06-16 Begin
@@ -754,31 +741,26 @@ public :
                 lb=Branch_length(*b, *a, delta, r);    /* using formula (2)        */
                 lamda=Lamda(*a, *b, vab, delta, n, r); /* compute lambda* using (9)*/
                 #ifdef NJ_OMP
-                    #pragma omp parallel
+                    #pragma omp parallel for
                 #endif
+                for(i=1; i <= n; i++)
                 {
-                    #ifdef NJ_OMP
-                        #pragma omp for
-                    #endif
-                    for(i=1; i <= n; i++)
+                    if(!Emptied(i,delta) && (i != *a) && (i != *b))
                     {
-                        if(!Emptied(i,delta) && (i != *a) && (i != *b))
+                        int x, y; //JB Moved x,y declarations here
+                                  //   So they'll be per-thread
+                        if(*a > i)
                         {
-                            int x, y; //JB Moved x,y declarations here
-                                      //   So they'll be per-thread
-                            if(*a > i)
-                            {
-                                x=*a;
-                                y=i;
-                            }
-                            else
-                            {
-                                x=i;
-                                y=*a;                           /* apply reduction formulae */
-                            }                                  /* 4 and 10 to delta        */
-                            delta[x][y]=Reduction4(*a, la, *b, lb, i, lamda, delta);
-                            delta[y][x]=Reduction10(*a, *b, i, lamda, vab, delta);
+                            x=*a;
+                            y=i;
                         }
+                        else
+                        {
+                            x=i;
+                            y=*a;                           /* apply reduction formulae */
+                        }                                  /* 4 and 10 to delta        */
+                        delta[x][y]=Reduction4(*a, la, *b, lb, i, lamda, delta);
+                        delta[y][x]=Reduction10(*a, *b, i, lamda, vab, delta);
                     }
                 }
                 strcpy(chain1,"");                     /* agglomerate the subtrees */
@@ -836,4 +818,33 @@ public :
     }
 };
 
-#endif
+//JB2020-06-26 Begin - Adapter, so that BioNj is available
+//for doing tree construction (via -starttree BIONJ2009).
+class BIONJ2009Adapter: public StartTree::BuilderInterface {
+protected:
+    std::string name = "BIONJ2009";
+    std::string description = "The reference (2009) version of BIONJ (with OMP parallelization)";
+public:
+    BIONJ2009Adapter() {
+    }
+    virtual const std::string& getName() {
+        return name;
+    }
+    virtual const std::string& getDescription() {
+        return description;
+    }
+    virtual void constructTree
+        ( const std::string &distanceMatrixFilePath
+         , const std::string & newickTreeFilePath) {
+            BioNj bio2009;
+            bio2009.create(distanceMatrixFilePath.c_str(), newickTreeFilePath.c_str());
+    }
+};
+
+namespace StartTree {
+    void addBioNJ2009TreeBuilders(Factory& f) {
+        f.advertiseTreeBuilder(new BIONJ2009Adapter());
+    }
+}
+//JB2020-06-26 Finish
+

utils/bionj2.cpp
@@ -6,18 +6,24 @@
 //  BIONJ implementation based on http://www.lirmm.fr/~w3ifa/MAAS/BIONJ/BIONJ.html
 //        (see BIONJMatrix).  Original authors: Olivier Gascuel
 //        and Hoa Sien Cuong (the code for the Unix version).
+//        Paper: "BIONJ: An Improved Version of the NJ Algorithm
+//                Based on a Simple Model of Sequence Data" (2009).
+//        Tag:   [GAS2009].
 //  NJ    implementation based on the same (but original NJ, without
 //        a matrix of variance estimates (see NJMatrix).
+//        Paper: "The neighbor-joining method: a new method
+//                for reconstructing phylogenetic trees" (1987).
+//        Tag:   [NS1987]
 //  BoundingNJ implementation loosely based on ideas from
-//        https://birc.au.dk/software/rapidnj/ and
-//        from: Inference of Large Phylogenies using Neighbour-Joining.
-//              Martin Simonsen, Thomas Mailund, Christian N. S. Pedersen.
-//              Communications in Computer and Information Science
-//              (Biomedical Engineering Systems and Technologies:
-//              3rd International Joint Conference, BIOSTEC 2010,
-//              Revised Selected Papers), volume 127, pages 334-344,
-//              Springer Verlag, 2011.
-//         Tag: [SMP2011].
+//        https://birc.au.dk/software/rapidnj/.
+//        Paper: Inference of Large Phylogenies using Neighbour-Joining.
+//               Martin Simonsen, Thomas Mailund, Christian N. S. Pedersen.
+//               Communications in Computer and Information Science
+//               (Biomedical Engineering Systems and Technologies:
+//               3rd International Joint Conference, BIOSTEC 2010,
+//               Revised Selected Papers), volume 127, pages 334-344,
+//               Springer Verlag, 2011.
+//         Tag:  [SMP2011].
 //         (but using a variance matrix, as in BIONJ, and keeping the
 //          distance and variance matrices square - they're not triangular
 //          because (i) *read* memory access patterns are more favourable
@@ -29,27 +35,27 @@
 //          those that has distance matrix rows identical to earlier rows;
 //          Rapid NJ "hates" them) (this is also covered in section 2.5)
 //         See the BoundingBIONJMatrix class.
+//   The vectorized implementations (of BIONJ and NJ) use Agner Fog's
+//   vectorclass library.
 //
 //  Created by James Barbetti on 18/6/2020.
 //
 
-#include "bionj2.h"
+#include "starttree.h"
 #include "heapsort.h"             //for mirroredHeapsort
 #include <vector>
 #include <string>
 #include <fstream>
 #include <iostream>               //for std::istream
 #include <boost/scoped_array.hpp> //for boost::scoped_array
-#include "utils/timeutil.h"       //for getRealTime()
 #include <vectorclass/vectorclass.h> //for Vec4d
 
 typedef double NJFloat;
 
 const NJFloat infiniteDistance = 1e+300;
 
-namespace
+namespace StartTree
 {
-
 
 struct Position
 {
@@ -423,7 +429,7 @@ public:
             , rowToCluster[2], halfD02 + halfD12 - halfD01);
         n = 0;
     }
-    virtual void doClustering() {
+    virtual void constructTree() {
         while (3<n) {
             Position best;
             getMinimumEntry(best);
@@ -613,7 +619,7 @@ public:
     BoundingMatrix(const std::string &distanceFilePath)
     : super(distanceFilePath) {
     }
-    virtual void doClustering() {
+    virtual void constructTree() {
         //1. Set up vectors indexed by cluster number,
         clusterToRow.resize(n);
         clusterTotals.resize(n);
@@ -862,19 +868,24 @@ public:
     }
 };
 
-class VectorizedBIONJMatrix: public BIONJMatrix
+template <class super=BIONJMatrix, class V=Vec4d, class VB=Vec4db>
+    class VectorizedMatrix: public super
 {
     //
     //Note: this is a first attempt at hand-vectorizing
     //      BIONJMatrix::getRowMinima (via Agner Fog's
     //      vectorclass library).
     //
+    using super::n;
+    using super::rows;
+    using super::rowMinima;
+    using super::rowTotals;
 public:
-    typedef BIONJMatrix super;
     mutable std::vector<NJFloat> scratchTotals;
     mutable std::vector<NJFloat> scratchColumnNumbers;
     size_t  blockSize;
-    VectorizedBIONJMatrix(const std::string &distanceMatrixFilePath)
+    
+    VectorizedMatrix(const std::string &distanceMatrixFilePath)
     : super(distanceMatrixFilePath) {
         size_t fluff = MATRIX_ALIGNMENT / sizeof(NJFloat);
         scratchTotals.resize(n + fluff, 0.0);
@@ -898,21 +909,21 @@ public:
             Position pos(row, 0, infiniteDistance);
             const NJFloat* rowData = rows[row];
             size_t col;
-            Vec4d minVector  ( infiniteDistance, infiniteDistance
-                             , infiniteDistance, infiniteDistance);
+            V minVector  ( infiniteDistance, infiniteDistance
+                          , infiniteDistance, infiniteDistance);
                 //The minima of columns with indices "congruent modulo 4"
                 //For example minVector[1] holds the minimum of
                 //columns 1,5,9,13,17,...
-            Vec4d ixVector(-1,     -1,     -1,     -1);
+            V ixVector(-1,     -1,     -1,     -1);
                 //For each entry in minVector, the column from which
                 //that value came.
             
             for (col=0; col+blockSize<row; col+=blockSize) {
-                Vec4d  rowVector; rowVector.load_a(rowData+col);
-                Vec4d  totVector; totVector.load_a(tot+col);
-                Vec4d  adjVector = rowVector - totVector;
-                Vec4db less      = adjVector < minVector;
-                Vec4d  numVector; numVector.load_a(nums+col);
+                V  rowVector; rowVector.load_a(rowData+col);
+                V  totVector; totVector.load_a(tot+col);
+                V  adjVector = rowVector - totVector;
+                VB less      = adjVector < minVector;
+                V  numVector; numVector.load_a(nums+col);
                 ixVector  = select(less, numVector, ixVector);
                 minVector = select(less, adjVector, minVector);
             }
@@ -936,42 +947,17 @@ public:
     }
 };//end of class
 
-} //end of anonymous namespace
+typedef BoundingMatrix<NJMatrix>      RapidNJ;
+typedef BoundingMatrix<BIONJMatrix>   RapidBIONJ;
+typedef VectorizedMatrix<NJMatrix>    VectorNJ;
+typedef VectorizedMatrix<BIONJMatrix> VectorBIONJ;
 
-void BIONJ2::constructTree
-    ( const std::string & distanceMatrixFilePath
-    , const std::string & newickTreeFilePath)
-{
-    BIONJMatrix d(distanceMatrixFilePath);
-    double joinStart = getRealTime();
-    d.doClustering();
-    double joinElapsed = getRealTime() - joinStart;
-    std::cout.precision(6);
-    std::cout << "Elapsed time for neighbour joining proper (in BIONJ2), " << joinElapsed << std::endl;
-    std::cout.precision(3);
-    d.writeTreeFile(newickTreeFilePath);
+void addBioNJ2020TreeBuilders(Factory& f) {
+    f.advertiseTreeBuilder( new Builder<NJMatrix>    ("NJ",      "Neighbour Joining (Saitou, Nei [1987])"));
+    f.advertiseTreeBuilder( new Builder<RapidNJ>     ("NJ-R",    "Rapid Neighbour Joining (Simonsen, Mailund, Pedersen [2011])"));
+    f.advertiseTreeBuilder( new Builder<VectorNJ>    ("NJ-V",    "Vectorized Neighbour Joining (Saitou, Nei [1987])"));
+    f.advertiseTreeBuilder( new Builder<BIONJMatrix> ("BIONJ",   "BIONJ (Gascuel, Cong [2009])"));
+    f.advertiseTreeBuilder( new Builder<RapidBIONJ>  ("BIONJ-R", "Rapid BIONJ (Saitou, Nei [1987], Gascuel [2009], Simonson Mailund Pedersen [2011])"));
+    f.advertiseTreeBuilder( new Builder<VectorBIONJ> ("BIONJ-V", "Vectorized BIONJ (Gascuel, Cong [2009])"));
 }
-
-void BIONJ2::constructTreeRapid
-    ( const std::string & distanceMatrixFilePath
-    , const std::string & newickTreeFilePath)
-{
-    BoundingMatrix<BIONJMatrix> d(distanceMatrixFilePath);
-    double joinStart = getRealTime();
-    d.doClustering();
-    double joinElapsed = getRealTime() - joinStart;
-    std::cout.precision(6);
-    std::cout << "Elapsed time for neighbour joining proper (in BIONJ2/rapidNJ), "
-        << joinElapsed << std::endl;
-    std::cout.precision(3);
-    d.writeTreeFile(newickTreeFilePath + "_2");
-
-    VectorizedBIONJMatrix d2(distanceMatrixFilePath);
-    joinStart = getRealTime();
-    d2.doClustering();
-    joinElapsed = getRealTime() - joinStart;
-    std::cout.precision(6);
-    std::cout << "Elapsed time for neighbour joining proper (in BIONJ2/Hand-Vectorized), " << joinElapsed << std::endl;
-    std::cout.precision(3);
-    d2.writeTreeFile(newickTreeFilePath + "_v");
-}
+}; //end of namespace

utils/bionj2.h
@@ -1,24 +1,0 @@
-//
-//  bionj2.h
-//  alignment
-//
-//  Created by James Barbetti on 18/6/20.
-//
-
-#ifndef bionj2_h
-#define bionj2_h
-
-#include <string>
-
-class BIONJ2
-{
-public:
-    void constructTree
-        ( const std::string &distanceMatrixFilePath
-        , const std::string & newickTreeFilePath);
-    void constructTreeRapid
-        ( const std::string &distanceMatrixFilePath
-        , const std::string & newickTreeFilePath);
-};
-
-#endif /* bionj2_hpp */

utils/starttree.cpp
@@ -1,0 +1,64 @@
+//
+//  starttree.cpp
+//  alignment
+//
+//  Created by James Barbetti on 26/6/20.
+//
+
+#include "starttree.h"
+#include <iostream>
+
+namespace StartTree {
+
+extern void addBioNJ2009TreeBuilders(Factory& f);
+extern void addBioNJ2020TreeBuilders(Factory& f);
+
+Factory::Factory() {
+}
+
+Factory::~Factory() {
+    for (auto it=mapOfTreeBuilders.begin(); it!=mapOfTreeBuilders.end(); ++it) {
+        delete it->second;
+    }
+    mapOfTreeBuilders.clear();
+}
+
+size_t Factory::getBuilderCount() {
+    return mapOfTreeBuilders.size();
+}
+
+Factory& Factory::getInstance() {
+    static Factory instance;
+    if (instance.getBuilderCount()==0) {
+        addBioNJ2009TreeBuilders(instance);
+        addBioNJ2020TreeBuilders(instance);
+    }
+    return instance;
+}
+
+void Factory::addBuilder(const std::string& name, BuilderInterface* builder) {
+    mapOfTreeBuilders [ name ] = builder;
+}
+
+BuilderInterface* Factory::getBuilder(const std::string& name) {
+    auto found = mapOfTreeBuilders.find(name);
+    return ( found == mapOfTreeBuilders.end())
+            ? nullptr
+            : found->second;
+}
+
+BuilderInterface* Factory::getBuilder(const char* name) {
+    std::string s(name);
+    return getBuilder(s);
+}
+
+void Factory::advertiseTreeBuilder(BuilderInterface* builder) {
+    std::string name = builder->getName();
+    addBuilder( name, builder );
+}
+
+BuilderInterface* Factory::getTreeBuilderByName(const std::string& name) {
+    return getInstance().getBuilder(name);
+}
+
+};

commit 7c8b9944a7998b3d323880882661299270f97203

computeMLDist now copies distance and variance matrices onto the PhyloTree before it asks it to write a distance file. Formerly it was writing out the old distance matrix (if there was one), or crashing (if there wasn't an old distance matrix). This change corrects a bug that I introduced in change 9af0c6a on 26th June.

main/phyloanalysis.cpp
@@ -1636,18 +1636,6 @@ void computeMLDist ( Params& params, IQTree& iqtree
     cout << "Computing ML distances took "
         << (getRealTime() - begin_wallclock_time) << " sec (of wall-clock time) "
         << (getCPUTime() - begin_cpu_time) << " sec(of CPU time)" << endl;
-    if (!params.dist_file)
-    {
-        iqtree.printDistanceFile();
-    }
-    double max_genetic_dist = MAX_GENETIC_DIST;
-    if (iqtree.aln->seq_type == SEQ_POMO) {
-        int N = iqtree.aln->virtual_pop_size;
-        max_genetic_dist *= N * N;
-    }
-    if (longest_dist > max_genetic_dist * 0.99) {
-        outWarning("Some pairwise ML distances are too long (saturated)");
-    }
     int n = iqtree.aln->getNSeq();
     int nSquared = n*n;
     if ( iqtree.dist_matrix == nullptr ) {
@@ -1665,6 +1653,18 @@ void computeMLDist ( Params& params, IQTree& iqtree
         memmove(iqtree.var_matrix, ml_var,
                 sizeof(double) * nSquared);
         delete[] ml_var;
+    }
+    if (!params.dist_file)
+    {
+        iqtree.printDistanceFile();
+    }
+    double max_genetic_dist = MAX_GENETIC_DIST;
+    if (iqtree.aln->seq_type == SEQ_POMO) {
+        int N = iqtree.aln->virtual_pop_size;
+        max_genetic_dist *= N * N;
+    }
+    if (longest_dist > max_genetic_dist * 0.99) {
+        outWarning("Some pairwise ML distances are too long (saturated)");
     }
 }
 