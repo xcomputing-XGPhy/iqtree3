a7b6675fa81553bf25c71f03b6f7913d035925bc
Thomas Wong <thomaskf@gmail.com>
2024-06-21 17:15:10 +1000

    add a function "generate_random_tree" for iqtree lib file

--- main/alisim.cpp
+++ main/alisim.cpp
@@ -218,13 +218,28 @@ int computeTotalSequenceLengthAllPartitions(PhyloSuperTree *super_tree)
 */
 void generateRandomTree(Params &params)
 {
+    if (!params.user_file) {
+        outError("Please specify an output tree file name");
+    }
+    try {
+        ofstream out;
+        out.open(params.user_file);
+        generateRandomTree(params, out);
+        out.close();
+    } catch (ios::failure) {
+        outError(ERR_WRITE_OUTPUT, params.user_file);
+    }
+}
+
+/**
+*  generate a random tree
+*/
+void generateRandomTree(Params &params, ostream &out)
+{
     if (params.sub_size < 3 && !params.aln_file) {
         outError(ERR_FEW_TAXA);
     }
 
-    if (!params.user_file) {
-        outError("Please specify an output tree file name");
-    }
     ////cout << "Random number seed: " << params.ran_seed << endl << endl;
 
     SplitGraph sg;
@@ -238,8 +253,8 @@ void generateRandomTree(Params &params)
                 string tmp_str(params.user_file);
                 outError(tmp_str + " exists. Use `-redo` option if you want to overwrite it.");
             }
-            ofstream out;
-            out.open(params.user_file);
+            // ofstream out;
+            // out.open(params.user_file);
             MTree itree;
 
             if (params.second_tree) {
@@ -268,7 +283,7 @@ void generateRandomTree(Params &params)
             default: break;
             }
             ofstream out2;
-            if (params.num_zero_len) {
+            if (params.num_zero_len && params.user_file != "") {
                 cout << "Setting " << params.num_zero_len << " internal branches to zero length..." << endl;
                 string str = params.user_file;
                 str += ".collapsed";
@@ -293,11 +308,13 @@ void generateRandomTree(Params &params)
                 mtree.printTree(out);
                 out << endl;
             }
-            out.close();
-            cout << params.repeated_time << " tree(s) printed to " << params.user_file << endl;
-            if (params.num_zero_len) {
-                out2.close();
-                cout << params.repeated_time << " collapsed tree(s) printed to " << params.user_file << ".collapsed" << endl;
+            // out.close();
+            if (params.user_file != "") {
+                cout << params.repeated_time << " tree(s) printed to " << params.user_file << endl;
+                if (params.num_zero_len) {
+                    out2.close();
+                    cout << params.repeated_time << " collapsed tree(s) printed to " << params.user_file << ".collapsed" << endl;
+                }
             }
         }
         // Generate random trees if optioned
@@ -323,7 +340,9 @@ void generateRandomTree(Params &params)
     } catch (bad_alloc) {
         outError(ERR_NO_MEMORY);
     } catch (ios::failure) {
-        outError(ERR_WRITE_OUTPUT, params.user_file);
+        string str = params.user_file;
+        str += ".collapsed";
+        outError(ERR_WRITE_OUTPUT, str);
     }
 
     // calculate the distance
