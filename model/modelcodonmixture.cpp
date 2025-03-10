41d33a626a00585c9a5c959fbe033691b1041e16
Minh Bui <m.bui@anu.edu.au>
2025-03-10 20:12:55 +1100

    Allow syntax +CMIX for codon mixture, e.g., GY+CMIX3 is equivalent to MIX{GY,GY,GY}. More implementation needed

---
+++ model/modelcodonmixture.cpp
@@ -0,0 +1,8 @@
+//
+//  modelcodonmixture.cpp
+//  iqtree
+//
+//  Created by Minh Bui on 4/3/2025.
+//
+
+#include "modelcodonmixture.hpp"
---
+++ model/modelcodonmixture.h
@@ -0,0 +1,13 @@
+//
+//  modelcodonmixture.hpp
+//  iqtree
+//
+//  Created by Minh Bui on 4/3/2025.
+//
+
+#ifndef modelcodonmixture_hpp
+#define modelcodonmixture_hpp
+
+#include <stdio.h>
+
+#endif /* modelcodonmixture_hpp */
