86fcc518fe9996780f955749fce9e68078653b21
NhanLT <trongnhan.uit@gmail.com>
2025-04-09 16:42:12 +1000

    update scripts to generate the Linux universal file

--- test_scripts/linux_universal.sh
+++ test_scripts/linux_universal.sh
@@ -1,46 +1,17 @@
 #!/bin/bash
 
 ARCH=$(uname -m)
-TMPDIR=$(mktemp -d)
 BINARY=""
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 if [[ "$ARCH" == "x86_64" ]]; then
-    BINARY="iqtree3_intel"
+    BINARY=".iqtree3_intel"
 elif [[ "$ARCH" == "aarch64" ]]; then
-    BINARY="iqtree3_arm"
+    BINARY=".iqtree3_arm"
 else
     echo "Unsupported architecture: $ARCH"
     exit 1
 fi
 
-# Find the byte offset where the binary blob starts
-M1="__BINARY"
-M2="_START__"
-MARKER="$M1$M2"
-OFFSET=$(grep -a -b "$MARKER" "$0" | head -n1 | cut -d: -f1)
-if [[ -z "$OFFSET" ]]; then
-    echo "Binary marker not found."
-    exit 1
-fi
-
-START_BYTE=$((OFFSET + ${#MARKER} + 2))
-
-# Extract the tar.gz archive from the script and unpack
-# Dump zip data to a temporary file
-TMPZIP=$(mktemp)
-tail -c +$START_BYTE "$0" > "$TMPZIP"
-
-# Unzip the temporary file
-unzip -o -q -d "$TMPDIR" "$TMPZIP"
-
-# Clean up
-rm -f "$TMPZIP"
-
-# Trap EXIT signal to clean up temporary directory
-trap 'rm -rf "$TMPDIR"' EXIT
-
-# Run the extracted binary
-"$TMPDIR/$BINARY" "$@"
-
-# clean up
-exit
+# Run the appropriate binary
+exec "${SCRIPT_DIR}/$BINARY" "$@"
--- test_scripts/make_linux_universal.sh
+++ test_scripts/make_linux_universal.sh
@@ -15,26 +15,16 @@ mkdir -p $universalname
 mkdir -p $universalname/bin
 cp $intelname/*.* $universalname
 
-cp $intelname/bin/iqtree3 iqtree3_intel
-cp $armname/bin/iqtree3 iqtree3_arm
-
-# Create a zip containing both binaries
-zip -vr binaries.zip iqtree3_intel iqtree3_arm
-
-# Append the marker and the tar.gz to the script
-cp linux_universal.sh tmp_linux_universal.sh
-echo "__BINARY_START__" >> tmp_linux_universal.sh
-cat  binaries.zip >> tmp_linux_universal.sh
+cp $intelname/bin/iqtree3 $universalname/bin/.iqtree3_intel
+cp $armname/bin/iqtree3 $universalname/bin/.iqtree3_arm
 
 # Make the final file executable
-chmod +x tmp_linux_universal.sh
-mv tmp_linux_universal.sh $universalname/bin/iqtree3
+chmod +x linux_universal.sh
+cp linux_universal.sh $universalname/bin/iqtree3
 
-rm binaries.zip
+
 rm -rf $intelname
 rm -rf $armname
-rm iqtree3_intel
-rm iqtree3_arm
 
 tar czf $universalname.tar.gz $universalname
 
