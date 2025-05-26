17b6a5a616c9c31366b17479dda82e63ccf39fdf
thomaskf <thomaskf@gmail.com>
2025-05-26 18:30:17 +1000

    change the make-dll-lib.bat to iqtree3

--- test_scripts/make-dll-lib.bat
+++ test_scripts/make-dll-lib.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 :: Build the DLL file and the DEF file
-g++ -shared -o iqtree2.dll %1 -Wl,--output-def,iqtree2.def -liqtree2 -L. -lws2_32 -lz -lpthread
+g++ -shared -o iqtree3.dll %1 -Wl,--output-def,iqtree3.def -liqtree3 -L. -lws2_32 -lz -lpthread
 
 :: Create the LIB file
-lib /def:iqtree2.def
+lib /def:iqtree3.def
