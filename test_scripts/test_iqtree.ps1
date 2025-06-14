1cccc1deddb23cf0aba61d93b0f21bef14027f88
Hashara <kumarasinghe.k.a.s.h@gmail.com>
2025-06-15 01:11:36 +1000

    variable name fix in ps1

--- test_scripts/test_iqtree.ps1
+++ test_scripts/test_iqtree.ps1
@@ -37,38 +37,35 @@ Get-Content test_scripts/test_data/turtle.fa.treefile, test_scripts/test_data/tu
 ## amino acid test cases
 # the data set is a subset of the turtle data set
 echo "Running amino acid test cases..."
-$AA_FASTA=test_scripts/test_data/turtle_aa.fasta
-$AA_NEX=test_scripts/test_data/turtle_aa.nex
-$AA_prefix=test_scripts/test_data/turtle_aa
 
-./build/iqtree3  -s $AA_FASTA -B 1000 -T 1 -seed $SEED
+./build/iqtree3  -s test_scripts/test_data/turtle_aa.fasta -B 1000 -T 1 -seed $SEED
 
-./build/iqtree3  -s $AA_FASTA -p $AA_NEX -B 1000 -T 1 -seed $SEED
+./build/iqtree3  -s test_scripts/test_data/turtle_aa.fasta -p test_scripts/test_data/turtle_aa.nex -B 1000 -T 1 -seed $SEED
 
-./build/iqtree3  -s $AA_FASTA -p $AA_NEX -B 1000 -T 1 -m MFP+MERGE -rcluster 10 --prefix test_scripts/test_data/turtle_aa.merge -seed $SEED
+./build/iqtree3  -s test_scripts/test_data/turtle_aa.fasta -p test_scripts/test_data/turtle_aa.nex -B 1000 -T 1 -m MFP+MERGE -rcluster 10 --prefix test_scripts/test_data/turtle_aa.merge -seed $SEED
 
-Get-Content $AA_FASTA.treefile, $AA_NEX.treefile |
+Get-Content test_scripts/test_data/turtle_aa.fasta.treefile, test_scripts/test_data/turtle_aa.nex.treefile |
     Set-Content test_scripts/test_data/turtle_aa.trees
-./build/iqtree3  -s $AA_FASTA -p test_scripts/test_data/turtle_aa.merge.best_scheme.nex -z test_scripts/test_data/turtle_aa.trees -zb 10000 -au -n 0 --prefix test_scripts/test_data/turtle_aa.test -seed $SEED -T 1
+./build/iqtree3  -s test_scripts/test_data/turtle_aa.fasta -p test_scripts/test_data/turtle_aa.merge.best_scheme.nex -z test_scripts/test_data/turtle_aa.trees -zb 10000 -au -n 0 --prefix test_scripts/test_data/turtle_aa.test -seed $SEED -T 1
 
-./build/iqtree3  -s $AA_FASTA -m GTR+F+I+R3+T -te test_scripts/test_data/turtle_aa.trees -T 1 --prefix test_scripts/test_data/turtle_aa.mix -seed $SEED
+./build/iqtree3  -s test_scripts/test_data/turtle_aa.fasta -m GTR+F+I+R3+T -te test_scripts/test_data/turtle_aa.trees -T 1 --prefix test_scripts/test_data/turtle_aa.mix -seed $SEED
 
-./build/iqtree3  -s $AA_FASTA -p $AA_NEX.best_scheme.nex -z test_scripts/test_data/turtle_aa.trees -n 0 -wpl --prefix test_scripts/test_data/turtle_aa.wpl -seed $SEED -T 1
+./build/iqtree3  -s test_scripts/test_data/turtle_aa.fasta -p test_scripts/test_data/turtle_aa.nex.best_scheme.nex -z test_scripts/test_data/turtle_aa.trees -n 0 -wpl --prefix test_scripts/test_data/turtle_aa.wpl -seed $SEED -T 1
 
-./build/iqtree3  -s $AA_FASTA -S $AA_NEX --prefix test_scripts/test_data/turtle_aa.loci -T 1 -seed $SEED
+./build/iqtree3  -s test_scripts/test_data/turtle_aa.fasta -S test_scripts/test_data/turtle_aa.nex --prefix test_scripts/test_data/turtle_aa.loci -T 1 -seed $SEED
 
-./build/iqtree3  -t $AA_NEX.treefile --gcf test_scripts/test_data/turtle_aa.loci.treefile -s $AA_FASTA --scf 100 -seed $SEED -T 1
+./build/iqtree3  -t test_scripts/test_data/turtle_aa.nex.treefile --gcf test_scripts/test_data/turtle_aa.loci.treefile -s test_scripts/test_data/turtle_aa.fasta --scf 100 -seed $SEED -T 1
 
-./build/iqtree3  -t $AA_FASTA.treefile --gcf test_scripts/test_data/turtle_aa.loci.treefile -s $AA_FASTA --scf 100 -seed $SEED -T 1
+./build/iqtree3  -t test_scripts/test_data/turtle_aa.fasta.treefile --gcf test_scripts/test_data/turtle_aa.loci.treefile -s test_scripts/test_data/turtle_aa.fasta --scf 100 -seed $SEED -T 1
 
 # link-exchange-rates model
 
-./build/iqtree3  -s $AA_FASTA -m "MIX{LG+F,LG+F}" --link-exchange-rates --prefix test_scripts/test_data/turtle_aa.mix.link -seed $SEED -T 1
+./build/iqtree3  -s test_scripts/test_data/turtle_aa.fasta -m "MIX{LG+F,LG+F}" --link-exchange-rates --prefix test_scripts/test_data/turtle_aa.mix.link -seed $SEED -T 1
 
-#./build/iqtree3  -s $AA_FASTA -m "MIX{GTR{1,1,1,1,1,1}+FO,GTR{1,1,1,1,1,1}+FO}" --link-exchange-rates --prefix test_scripts/test_data/turtle_aa.mix.jc.link -seed $SEED -T 1
+#./build/iqtree3  -s test_scripts/test_data/turtle_aa.fasta -m "MIX{GTR{1,1,1,1,1,1}+FO,GTR{1,1,1,1,1,1}+FO}" --link-exchange-rates --prefix test_scripts/test_data/turtle_aa.mix.jc.link -seed $SEED -T 1
 
-./build/iqtree3  -s $AA_FASTA -p $AA_NEX -g test_scripts/test_data/turtle.constr.tree --prefix $AA_NEX.constr -T 1 -seed $SEED
+./build/iqtree3  -s test_scripts/test_data/turtle_aa.fasta -p test_scripts/test_data/turtle_aa.nex -g test_scripts/test_data/turtle.constr.tree --prefix test_scripts/test_data/turtle_aa.nex.constr -T 1 -seed $SEED
 
-./build/iqtree3  -s $AA_FASTA -p $AA_NEX -g test_scripts/test_data/turtle.constr.tree2 -B 1000 -alrt 1000 --prefix $AA_NEX.constr2 -T 1 -seed $SEED
+./build/iqtree3  -s test_scripts/test_data/turtle_aa.fasta -p test_scripts/test_data/turtle_aa.nex -g test_scripts/test_data/turtle.constr.tree2 -B 1000 -alrt 1000 --prefix test_scripts/test_data/turtle_aa.nex.constr2 -T 1 -seed $SEED
 
-#./build/iqtree3  -s $AA_FASTA -m "MIX+MF" --prefix test_scripts/test_data/turtle_aa.mixfinder -T 1 -seed $SEED # mixture finder is not supported for amino acid data
+#./build/iqtree3  -s test_scripts/test_data/turtle_aa.fasta -m "MIX+MF" --prefix test_scripts/test_data/turtle_aa.mixfinder -T 1 -seed $SEED # mixture finder is not supported for amino acid data
